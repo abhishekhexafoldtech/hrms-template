b4984f3bd37bb98dc84683638ae28b3ace252588
Abhishek Anand <abhishek.anand@hexafoldtech.com>
2023-07-20 10:26:49 +0530

    boiler plate

---
+++ pages/employee/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>employee</h1>
+    </div>
+</template>
+
+
+<script setup>
+definePageMeta({
+  layout: "layout",
+});
+</script>
---
+++ pages/organization/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>organization</h1>
+    </div>
+</template>
+
+
+<script setup>
+definePageMeta({
+  layout: "layout",
+});
+</script>
