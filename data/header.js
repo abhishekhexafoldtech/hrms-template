b70a1d9ee74cdef62f40e970980f305ae6fa665e
amulyajaiswal <136559817+amulyajaiswal@users.noreply.github.com>
2023-07-20 12:09:48 +0530

    Change in header UI

---
+++ data/header.js
@@ -0,0 +1,147 @@
+const headerData = {
+    org: [
+        {
+            id: 1,
+            name: "Overview",
+            description: "Overview is here all overview put here ",
+            url: "",
+        },
+        {
+            id: 2,
+            name: "Address",
+            description: "Address is here",
+            url: "",
+        },
+        {
+            id: 3,
+            name: "Policies",
+            description: "Policies are here",
+            url: "",
+        },
+    ],
+    project: [
+        {
+            id: 1,
+            name: "Project Details",
+            description: "Name is Here",
+            url: "",
+        },
+        {
+            id: 2,
+            name: "Credentials",
+            description: "Address is here",
+            url: "",
+        },
+        {
+            id: 3,
+            name: "Members",
+            description: "Policies are here",
+            url: "",
+        },
+    ],
+    interview: [
+        {
+            id: 1,
+            name: "Opening",
+            description: "Name are here",
+            url: "",
+        },
+        {
+            id: 2,
+            name: "Applications",
+            description: "Address is here",
+            url: "",
+        },
+        {
+            id: 3,
+            name: "Interview",
+            description: "Policies are here",
+            url: "",
+        },
+        {
+            id: 4,
+            name: "Onboarding",
+            description: "Policies are here",
+            url: "",
+        },
+        {
+            id: 5,
+            name: "Talent Pool",
+            description: "Policies are here",
+            url: "",
+        },
+    ],
+    teams: [
+        {
+            id: 1,
+            name: "Employee",
+            description: "Name is here",
+            url: "",
+        },
+        {
+            id: 2,
+            name: "Attendance",
+            description: "Address is here",
+            url: "",
+        },
+        {
+            id: 3,
+            name: "Leave",
+            description: "Policies are here",
+            url: "",
+        },
+        {
+            id: 4,
+            name: "Expenses",
+            description: "Policies are here",
+            url: "",
+        },
+    ],
+    payroll: [
+        {
+            id: 1,
+            name: "Payslip",
+            description: "Name is here",
+            url: "",
+        },
+        {
+            id: 2,
+            name: "Salary Structure",
+            description: "Address is here",
+            url: "",
+        },
+        {
+            id: 3,
+            name: "Tax Projection",
+            description: "Policies are here",
+            url: "",
+        },
+        {
+            id: 4,
+            name: "Declration",
+            description: "Policies are here",
+            url: "",
+        },
+        {
+            id: 5,
+            name: "Bank Account",
+            description: "Policies are here",
+            url: "",
+        },
+    ],
+    docs: [
+        {
+            id: 1,
+            name: "Folder",
+            description: "Name is here",
+            url: "",
+        },
+        {
+            id: 2,
+            name: "Files",
+            description: "Address is here",
+            url: "",
+        },
+    ],
+};
+export default headerData;
