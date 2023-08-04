48635f0eae131c445a3d942e07758ada0bc049e5
dattahexafold <dattatraya.ghadage@hexafoldtech.com>
2023-08-04 11:23:47 +0530

    event card added

---
+++ components/eventCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="card shadow-lg min-height-160 min-height-160">
+    <div class="mt-2 ms-2">
+      <h6 class="mb-0">Imp Link</h6>
+      <br />
+      <p class="text-secondary text-sm font-weight-normal">
+        <i class="fa fa-dot-circle-o" aria-hidden="true"></i> 1 Meeting with HR
+      </p>
+      <p class="text-secondary text-sm font-weight-normal">
+        <i class="fa fa-dot-circle-o" aria-hidden="true"></i> 1 Meeting with HR
+      </p>
+      <p class="text-secondary text-sm font-weight-normal">
+        <i class="fa fa-dot-circle-o" aria-hidden="true"></i> 1 Meeting with HR
+      </p>
+      <p class="text-secondary text-sm font-weight-normal">
+        <i class="fa fa-dot-circle-o" aria-hidden="true"></i> 1 Meeting with HR
+      </p>
+    </div>
+  </div>
+</template>
+<script setup></script>
+<style></style>
