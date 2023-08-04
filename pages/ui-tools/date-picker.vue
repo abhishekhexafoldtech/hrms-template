0af3ea472aa501ebf82c31551031fddd005c7d93
kirankumarhexafoldtech <kirankumar.challagiri@hexafoldtech.com>
2023-08-04 10:53:43 +0530

    date-picker-custom-component-v1

---
+++ pages/ui-tools/date-picker.vue
@@ -0,0 +1,13 @@
+<template>
+    <DatePicker @handleDate="handleDate" :disableYears="3" />
+</template>
+
+<script setup>
+const holidays = ref([
+    "01-08-2023"
+])
+function handleDate(date){
+    console.log("selected date",date)
+}
+</script>
+
